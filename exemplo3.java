2bede1bc4168f828af839c06173ec88f91ddc785
Leogomes07 <126675289+Leogomes07@users.noreply.github.com>
2023-04-14 13:51:42 -0300

    NÃO fique bravo gugus vc é um otimo professor

---
+++ exemplo3.java
@@ -0,0 +1,21 @@
+//cálculo da área do triangulo
+
+//Cálculo da área do triângulo
+
+
+
+
+public class Exemplo3{
+ public static final double BASE = 5.5; 
+ public static void main(String[] args) {
+
+    
+ double altura =4.0;
+ double area =(BASE*altura)/2;
+System.out.println("A área do triângulo é:" +area+"cm²");
+    
+    }
+    }
+
+
+
